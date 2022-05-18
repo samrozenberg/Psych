6c3c5bd5d33e572caf7850a5834420f267c5a50f
Sam Rozenberg <sam.rozen@hotmail.com>
2022-05-18 12:04:58 +0200

    changes complete

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -7,7 +7,14 @@ class EvaluationsController < ApplicationController
 
 
     if @evaluation.norm.name == "Digit Span - Empan Direct" || @evaluation.norm.name == "Digit Span - Empan Inversé"
-      @record = Record.where(norm: @evaluation.norm, age: @patient.age, study_level: @patient.study_level)[0]
+      if @patient.age > 79
+        @record = Record.where(norm: @evaluation.norm, age: 79, study_level: @patient.study_level)[0]
+      elsif @patient.age < 18
+        @record = Record.where(norm: @evaluation.norm, age: 18, study_level: @patient.study_level)[0]
+      else
+        @record = Record.where(norm: @evaluation.norm, age: @patient.age, study_level: @patient.study_level)[0]
+      end
+
       @result_outcome = ((@evaluation.score - @record.mean) / @record.standard_deviation.to_f).round(2)
 
       Result.create!(evaluation: @evaluation, outcome: @result_outcome, kind: "Score Z")

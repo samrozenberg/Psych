c6e0e8b1b9ae743458dd5a211e348246f287a136
Sam Rozenberg <sam.rozen@hotmail.com>
2022-10-18 20:22:32 +0200

    changes in commit button complete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require clipboard
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -14,3 +15,10 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+$(document).ready(function () {
+
+  var clipboard = new Clipboard('.clipboard-btn');
+  console.log(clipboard);
+
+});
